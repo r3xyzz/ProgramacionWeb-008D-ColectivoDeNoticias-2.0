37067f1504cb40bba27d6f38a4e253b2136e20d2
r3xyzz <rexgamer152@gmail.com>
2024-04-12 21:28:34 -0400

    Hoja de JavaScript (por probar) y el PopUp promocional

---
+++ Prog Web 2.0/animaciones.js
@@ -0,0 +1,10 @@
+// Hoja de JavaScript
+
+// Referencia al modal utilizando la ID
+var modal= document.getElementById('miModal');
+
+// Abertura del modal
+$('#miModal').modal('show')
+
+// Cierre del modal
+$('#miModal').modal('hide')
