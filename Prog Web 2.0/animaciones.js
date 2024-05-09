15ac93c07a83ddd321aabe7566702507a5409cd9
r3xyzz <rexgamer152@gmail.com>
2024-05-08 21:22:10 -0400

    NUEVO navar con fade in y fade out

--- Prog Web 2.0/animaciones.js
+++ Prog Web 2.0/animaciones.js
@@ -3,10 +3,19 @@
 // Implemetear Js con JQuery para tener las validaciones
 // https://youtu.be/0WXiaIpQk-Q?si=BST30CxTZRDbOz2h animaciones con JQuery para hacer
 
+//
 $(document).ready(function(){
-
- // Una vez listo el document.ready implementar todo codigo o logica dentro
-
-
-
+    $(document).ready(function(){
+        let ubicacionPrincipal = window.pageYOffset;
+        window.onscroll = function() {
+            let Desplazamiento_Actual = window.pageYOffset;
+            if(ubicacionPrincipal >= Desplazamiento_Actual){
+                $('#navbar').fadeIn(); // Cambio aquí, utiliza fadeIn()
+            }
+            else{
+                $('#navbar').fadeOut(); // Cambio aquí, utiliza fadeOut()
+            }
+            ubicacionPrincipal = Desplazamiento_Actual;
+        };
+    });
 });
