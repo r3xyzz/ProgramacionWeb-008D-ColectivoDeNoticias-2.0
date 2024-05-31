05bc7a903d58d7498405d7e860ba6a8afec0c877
r3xyzz <rexgamer152@gmail.com>
2024-05-31 09:00:27 -0400

    bug from noti JS

--- Prog Web 2.0/staticJS/FormNotiJS.js
+++ Prog Web 2.0/staticJS/FormNotiJS.js
@@ -1,4 +1,6 @@
+$(document).ready(function() {
 document.getElementById('imagen').addEventListener('change', function(event) {
     var imagePreview = document.getElementById('image-preview');
     imagePreview.src = URL.createObjectURL(event.target.files[0]);
   });
+});
