63ef8284537029b4450badd09a274d02aea8d854
ReDiego0 <re.diego.maldonado@gmail.com>
2024-06-10 18:04:50 -0400

    Agregado superusuario y agregadas tablas a admin

--- Noticias/admin.py
+++ Noticias/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Usuario, Categoria, Noticia, Suscripcion, ProductoAdicional, Carrito
 
 # Register your models here.
+
+admin.site.register(Usuario)
+admin.site.register(Categoria)
+admin.site.register(Noticia)
+admin.site.register(Suscripcion)
+admin.site.register(ProductoAdicional)
+admin.site.register(Carrito)
